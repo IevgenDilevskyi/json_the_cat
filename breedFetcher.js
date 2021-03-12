158f0776d071cbb6373f03b59d539f7dbd0aacfe
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-12 00:06:25 +0000

    completed assignment with unit tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
 const request = require('request');
+// const arg = process.argv.slice(2);
 
-const arg = process.argv.slice(2);
-const source = `https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`;
+// const breedName = arg[0];
+const fetchBreedDescription = function(breedName, callback) {
+  const source = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(source, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+    
+    if (data[0] === undefined) {
+      callback('Breed is not found', null);
+    } else {
+        callback(null, data[0].description);
+      }
+  });
 
-request(source, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log('Error occured. Error details:\n ', error);
-  } else if (data[0] === undefined) {
-    console.log('Breed is not found');
-  } else {
-    console.log(data[0].description);
-  // console.log(body);
-  // console.log(data);
-  // console.log(typeof data);
-  // console.log('statusCode:', response && response.statusCode);
-  // fs.writeFile(dest, body, () => {
-  //   console.log(`Downloaded and saved ${body.length} bytes to ${dest}`);
-  // });
-  }
-});
+};
+
+module.exports = { fetchBreedDescription };
